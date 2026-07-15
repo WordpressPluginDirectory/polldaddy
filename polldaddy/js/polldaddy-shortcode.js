7f4b89685e181b2c9dca84cc22eacb1b530f63f3
Justin Gardner <justin@rhynorater.com>
2026-07-15 06:04:24 -0500

    3.1.8

--- polldaddy/js/polldaddy-shortcode.js
+++ polldaddy/js/polldaddy-shortcode.js
@@ -8,21 +8,35 @@
 
 			if ( polls ){
 				$.each( polls, function() {
-					var poll = $( this ).data( 'settings' );
+					var poll_el = this;
+
+					// Only process each poll element once, even if render() runs
+					// again (e.g. on a later 'post-load' / 'pd-script-load' event).
+					if ( poll_el.getAttribute( 'data-pd-init-done' ) ) {
+						return;
+					}
+					poll_el.setAttribute( 'data-pd-init-done', '1' );
+
+					var poll = $( poll_el ).data( 'settings' );
 
 					if ( poll ) {
 						var poll_url = document.createElement("a");
 						poll_url.href = poll['url'];
+						// Skip this element with return, not return false: return false
+						// would break the whole $.each loop and hide later valid polls.
+						if ( poll_url.protocol !== 'https:' ) {
+							return;
+						}
 						if ( poll_url.hostname != 'secure.polldaddy.com' && poll_url.hostname != 'static.polldaddy.com' ) {
-							return false;
+							return;
 						}
 						var pathname = poll_url.pathname;
-						if ( ! /\/?p\/\d+\.js/.test( pathname ) ) {
-							return false;
+						if ( ! /^\/p\/\d+\.js$/.test( pathname ) ) {
+							return;
 						}
 						var wp_pd_js = document.createElement('script');
 						wp_pd_js.type = 'text/javascript';
-						wp_pd_js.src = poll['url'];
+						wp_pd_js.src = poll_url.href;
 						wp_pd_js.charset = 'utf-8';
 						wp_pd_js.async = true;
 						document.getElementsByTagName('head')[0].appendChild(wp_pd_js);
@@ -31,18 +45,39 @@
 			};
 
 			if ( ratings ){
-				var script = '';
-
 				$.each( ratings, function() {
-					var rating = $( this ).data( 'settings' );
+					var rating_el = this;
 
-					if ( rating ) {
-						script += "PDRTJS_settings_" + rating['id'] + rating['item_id'] + "=" + rating['settings'] + "; if ( typeof PDRTJS_RATING !== 'undefined' ){ if ( typeof PDRTJS_" + rating['id'] + rating['item_id'] + "=='undefined' ){PDRTJS_" + rating['id'] + rating['item_id'] + "= new PDRTJS_RATING( PDRTJS_settings_" + rating['id'] + rating['item_id'] + " );}}";
+					// Only process each rating element once.
+					if ( rating_el.getAttribute( 'data-pd-init-done' ) ) {
+						return;
+					}
+					rating_el.setAttribute( 'data-pd-init-done', '1' );
+
+					var rating = $( rating_el ).data( 'settings' );
+
+					if ( ! rating ) {
+						return;
+					}
+
+					// The 'settings' value is a JSON-encoded settings object built
+					// server-side (see polldaddy-shortcode.php). Parse it as data and
+					// assign it directly rather than concatenating it into a <script>,
+					// so the markup can only ever contribute data, never executable code.
+					var settings;
+					try {
+						settings = JSON.parse( rating['settings'] );
+					} catch ( e ) {
+						return;
+					}
+
+					var key = '' + rating['id'] + rating['item_id'];
+					window[ 'PDRTJS_settings_' + key ] = settings;
+
+					if ( typeof PDRTJS_RATING !== 'undefined' && typeof window[ 'PDRTJS_' + key ] === 'undefined' ) {
+						window[ 'PDRTJS_' + key ] = new PDRTJS_RATING( window[ 'PDRTJS_settings_' + key ] );
 					}
 				});
-
-				if ( script.length > 0 )
-					$( '#polldaddyRatings' ).after( "<script type='text/javascript' charset='utf-8' id='polldaddyDynamicRatings'>" + script +  "</script>" );
 			};
 		}
 	}
